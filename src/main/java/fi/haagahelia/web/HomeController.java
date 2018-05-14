321dc68b13e25e456d52b43e092e0ed764561591
osmartti <ossi.marttinen@gmail.com>
2018-05-14 12:37:59 +0300

    Updated the App.js and added link to create. Added also add new product as successful procedure.

--- src/main/java/fi/haagahelia/web/HomeController.java
+++ src/main/java/fi/haagahelia/web/HomeController.java
@@ -3,13 +3,10 @@ package fi.haagahelia.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import fi.haagahelia.domain.Student;
 import fi.haagahelia.domain.StudentRepository;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 @Controller
@@ -17,6 +14,7 @@ public class HomeController {
 
     @Autowired
     private StudentRepository repository;
+
     @RequestMapping("/login")
     public String login() {
         return "login";
@@ -26,15 +24,30 @@ public class HomeController {
     public String index() {
         return "index";
     }
-                //restfull haku listaan
+
+    @RequestMapping(value = "/create")
+    public String addStudent(Model model){
+        model.addAttribute("students", new Student());
+        return "create";
+    }
+
+    //restfull haku listaan
     @RequestMapping(value="/studentlist", method = RequestMethod.GET)
     public @ResponseBody
     List<Student> studentListRest() {
         return (List<Student>) repository.findAll();
     }
+
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteStudent(@PathVariable("id") Long id, Model model) {
         repository.delete(id);
         return "redirect:../";
     }
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+
+    public String save(Student student) {
+        repository.save(student);
+        return "redirect:/";
+    }
+
 }
